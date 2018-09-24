7b44b3e8eca8ae23e47dc3b8abfc23c939f11e18
Hemant <singh.hemant.edu@gmail.com>
2018-09-24 22:23:29 +0530

    TDD

---
+++ Mvc.TDD/BankApplication/BankApplication.BusinessLayer/LoanEligibility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankApplication.Contracts;
+
+namespace BankApplication.BusinessLayer
+{
+    public class LoanEligibility : ILoanEligibility
+    {
+        public bool HasCorrectType(string loanType)
+        {
+            if(loanType.Equals("Personal"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Mvc.TDD/BankApplication/BankApplication.Tests/LoanEligibilityTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankApplication.Contracts;
+
+namespace BankApplication.Tests
+{
+    [TestClass]
+    public class LoanEligibilityTest
+    {
+        private ILoanEligibility _loanEligibility;
+        public LoanEligibilityTest(ILoanEligibility loanEligibility)
+        {
+            _loanEligibility = loanEligibility;
+        }
+
+        [TestMethod]
+        public void TestLoanTypePersonal()
+        {
+            //Arrange
+            string loanType = "Personal";
+
+            //Act
+            bool expected = _loanEligibility.HasCorrectType(loanType);
+
+            //Assert
+            Assert.IsTrue(expected);
+        }
+    }
+}
